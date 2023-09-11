888b2ed488d98d8051e5676d19f4e3f372d99c09
Teolhyn <teemu.hynna@gmail.com>
2023-09-11 20:36:41 +0300

    Added Excel import and avg price calc.

---
+++ New_start.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def excelToDataframe(table: str):
+    df = pd.DataFrame(pd.read_csv(table, delimiter=";", header=0))
+    #Replace decimal ',' with '.'
+    df['Hinta'] = df['Hinta'].str.replace(',', '.').astype(float)
+    return df
+
+def averagePriceSince(dateBought: str, data: pd.DataFrame):
+    listOfIndexes = data.index[data['DateTime'].str.contains(dateBought)]
+    startFrom = int(listOfIndexes[-1])
+    averagePrice = data['Hinta'].iloc[startFrom:].mean()
+    return averagePrice
+
+if __name__ == "__main__":
+    date = "2023-04-16"
+    df = excelToDataframe("chart.csv")
+    avgPrice = averagePriceSince(date, df)
+    print("Keskimääräinen sähkön markkinahinta", date, "eteenpäin on:", "%.2f" % avgPrice, "snt/kWh")
